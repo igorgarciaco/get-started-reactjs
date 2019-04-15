5f09c79e565ac569a319d5ff846ffa22cca0f100
Igor Garcia <36867094+igorgarciaco@users.noreply.github.com>
2019-04-15 16:25:13 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+import Routes from './routes';
+
+import './styles.css';
+
+import Header from './components/header';
+import Main from './pages/main';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Routes />
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
---
+++ src/components/header/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import "./styles.css";
+// const Header = () => (
+//     <header id="mainHeader">JSHello</header>
+// );
+
+class Header extends Component {
+    render(){
+        return (
+            <header id="mainHeader">JSHello</header>
+        )
+    }
+}
+
+export default Header;
